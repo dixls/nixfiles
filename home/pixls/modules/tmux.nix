6a6cc1d0163cc50cb5be59f675a5f0c11c5c314a
pixls <dev@snack.supply>
2024-09-06 17:54:13 -0400

    disable tmux-nova for now

--- home/pixls/modules/tmux.nix
+++ home/pixls/modules/tmux.nix
@@ -11,41 +11,41 @@
     prefix = "C-s";
     plugins = with unstable; [
       tmuxPlugins.vim-tmux-navigator
-      {
-        plugin = tmuxPlugins.tmux-nova;
-        extraConfig = ''
-          set -g @nova-nerdfonts true
-          set -g @nova-nerdfonts-left 
-          set -g @nova-nerdfonts-right 
-          
-          foreground="#E9E0D2"
-          dark_bg="#08211B"
-          light_pink="#F5CABF"
-          light_green="#7AB49D"
-          dark_green="#1F4E43"
-          light_teal="#82B0A5"
+      # {
+      #   plugin = tmuxPlugins.tmux-nova;
+      #   extraConfig = ''
+      #     set -g @nova-nerdfonts true
+      #     set -g @nova-nerdfonts-left 
+      #     set -g @nova-nerdfonts-right 
+      #     
+      #     foreground="#E9E0D2"
+      #     dark_bg="#08211B"
+      #     light_pink="#F5CABF"
+      #     light_green="#7AB49D"
+      #     dark_green="#1F4E43"
+      #     light_teal="#82B0A5"
 
-          set -g @nova-segment-mode "#{?client_prefix,Ω,ω}"
-          set -g @nova-segment-mode-colors "$light_pink $dark_bg"
-          
-          set -g @nova-segment-whoami "#(whoami)@#h"
-          set -g @nova-segment-whoami-colors "$light_pink $dark_bg"
-          
-          set -g @nova-pane "#I#{?pane_in_mode,  #{pane_mode},}  #W"
-          
-          set -g @nova-rows 0
-          set -g @nova-segments-0-left "mode"
-          set -g @nova-segments-0-right "whoami"
+      #     set -g @nova-segment-mode "#{?client_prefix,Ω,ω}"
+      #     set -g @nova-segment-mode-colors "$light_pink $dark_bg"
+      #     
+      #     set -g @nova-segment-whoami "#(whoami)@#h"
+      #     set -g @nova-segment-whoami-colors "$light_pink $dark_bg"
+      #     
+      #     set -g @nova-pane "#I#{?pane_in_mode,  #{pane_mode},}  #W"
+      #     
+      #     set -g @nova-rows 0
+      #     set -g @nova-segments-0-left "mode"
+      #     set -g @nova-segments-0-right "whoami"
 
-          set -g @nova-pane-active-border-style "$light_teal"
-          set -g @nova-pane-border-style "$dark_bg"
-          set -g @nova-status-style-bg "$dark_bg"
-          set -g @nova-status-style-fg "$light_green"
-          set -g @nova-status-style-active-bg "$dark_green"
-          set -g @nova-status-style-active-fg "$foreground"
-          set -g @nova-status-style-double-bg "$dark_green"
-        '';
-      }
+      #     set -g @nova-pane-active-border-style "$light_teal"
+      #     set -g @nova-pane-border-style "$dark_bg"
+      #     set -g @nova-status-style-bg "$dark_bg"
+      #     set -g @nova-status-style-fg "$light_green"
+      #     set -g @nova-status-style-active-bg "$dark_green"
+      #     set -g @nova-status-style-active-fg "$foreground"
+      #     set -g @nova-status-style-double-bg "$dark_green"
+      #   '';
+      # }
     ];
     extraConfig = ''
       set-option -g status-position top
