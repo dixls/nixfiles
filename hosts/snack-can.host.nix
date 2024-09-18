0e438d3945ae86e0722343f1e8447695a567fd85
pixls <dev@snack.supply>
2024-09-18 14:42:55 -0400

    fix ip issues, add snack-can-specific prompt

--- hosts/snack-can.host.nix
+++ hosts/snack-can.host.nix
@@ -23,17 +23,17 @@
 
   networking = {
     networkmanager.enable = false;
-    interfaces.enp12s0.useDHCP = false;
-    interfaces.enp12s0.ipv4addresses = [
+    interfaces.enp12s0.useDHCP = true;
+    interfaces.enp12s0.ipv4.addresses = [
       {
         address = "192.168.1.15";
         prefixLength = 24;
       }
     ];
     interfaces.enp11s0.useDHCP = false;
-    interfaces.enp11s0.ipv4addresses = [
+    interfaces.enp11s0.ipv4.addresses = [
       {
-        address = "192.168.1.16";
+        address = "192.168.1.14";
         prefixLength = 24;
       }
     ];
