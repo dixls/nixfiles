b79e3a7e729b4894ba7fc86148c8498bdcfc5130
pixls <dev@snack.supply>
2024-09-20 17:33:32 -0400

    fix savory config

--- hosts/common.host.nix
+++ hosts/common.host.nix
@@ -41,7 +41,7 @@
     };
   };
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = lib.mkDefault true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.networkmanager.enable = lib.mkDefault true;
