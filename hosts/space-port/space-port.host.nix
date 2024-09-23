0df77897b3aa29b46c8c02c11f8002c4b8ce5ce7
pixls <dev@snack.supply>
2024-09-23 18:21:28 -0400

    working config and now has steam

--- hosts/space-port/space-port.host.nix
+++ hosts/space-port/space-port.host.nix
@@ -9,14 +9,16 @@
     };
   };
 
-  # services.xserver.enable = true;
+  services.xserver.enable = true;
 
   # Use this to pick which GUI and other bundles
   imports = [
-    ./../gui/desktop-defaults.nix
-    # ./../gui/swayfx # this apparently only works from home manager maybe? but some extra bits required
-    ./../gui/gnome
-    # ./../gui/plasma
+    ./hardware-configuration.nix
+
+    ./../../gui/desktop-defaults.nix
+    # ./../../gui/swayfx # this apparently only works from home manager maybe? but some extra bits required
+    ./../../gui/gnome
+    # ./../../gui/plasma
 
     # other bundles
     #./../system-modules/server-bundle.nix
@@ -25,21 +27,7 @@
   services.openssh.enable = true;
 
   networking = {
-    networkmanager.enable = false;
-    interfaces.enp12s0.useDHCP = true;
-    interfaces.enp12s0.ipv4.addresses = [
-      {
-        address = "192.168.1.15";
-        prefixLength = 24;
-      }
-    ];
-    interfaces.enp11s0.useDHCP = false;
-    interfaces.enp11s0.ipv4.addresses = [
-      {
-        address = "192.168.1.14";
-        prefixLength = 24;
-      }
-    ];
+    networkmanager.enable = true;
   };
 
   # Define a user account.
