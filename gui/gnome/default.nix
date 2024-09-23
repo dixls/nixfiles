67d09ad3486b88c81f87df1dd7ea3a3dc99137bb
pixls <dev@snack.supply>
2024-09-23 18:21:28 -0400

    working config and now has steam

--- gui/gnome/default.nix
+++ gui/gnome/default.nix
@@ -3,10 +3,10 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm = {
     enable = true;
-    wayland = true;
+    # wayland = true;
   };
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.defaultSession = "gnome";
+  # services.xserver.displayManager.defaultSession = "gnome";
 
   environment.gnome.excludePackages = (with pkgs; [
     # for packages that are pkgs.*
--- home/pixls/home.nix
+++ home/pixls/home.nix
@@ -30,6 +30,8 @@
     pkgs.fastfetch
     pkgs.foot
 
+    pkgs.steam
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
