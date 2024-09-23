4c6b115688942623636c9be3aaddd0a8654b8253
pixls <dev@snack.supply>
2024-09-23 19:18:51 -0400

    add some more packages

--- gui/desktop-defaults.nix
+++ gui/desktop-defaults.nix
@@ -15,6 +15,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
