cf46a726faf922aca7c2a5fe4ed5aa71d53c2b62
pixls <dev@snack.supply>
2024-10-10 22:27:47 -0400

    fix weird home-manager error

--- flake.nix
+++ flake.nix
@@ -113,6 +113,7 @@
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
+              home-manager.backupFileExtension = "backup";
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = {
                 inherit inputs;
