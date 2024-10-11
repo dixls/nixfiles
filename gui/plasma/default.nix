1eebdab9b04a45a41587c347fae624c364625a49
pixls <dev@snack.supply>
2024-10-10 20:25:11 -0400

    ok we're actually using plasma again i guess

--- gui/plasma/default.nix
+++ gui/plasma/default.nix
@@ -7,4 +7,9 @@
   };
   services.desktopManager.plasma6.enable = true;
 
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    konsole
+    plasma-browser-integration
+    oxygen
+  ];
 }
