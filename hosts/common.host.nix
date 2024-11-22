a0a9f7756eb60f4836f231fa974434cabd23a3dc
pixls <dev@snack.supply>
2024-11-21 19:24:47 -0500

    fix typo

--- hosts/common.host.nix
+++ hosts/common.host.nix
@@ -2,11 +2,11 @@
 
 {
   sops = {
-    defaultSopsFile = ./../../../secrets.yaml;
+    defaultSopsFile = ../secrets.yaml;
     validateSopsFiles = false;
 
     age = {
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      sshKeyPaths = [ "/etc/ssh/ssh_host-ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
