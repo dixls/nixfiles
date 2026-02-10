4cd1388ef3691e84885522efc76ff2533e7da582
pixls <dev@snack.supply>
2026-02-09 21:28:36 -0500

    add nextcloud initial config

--- system-modules/gideon-bundle.nix
+++ system-modules/gideon-bundle.nix
@@ -29,6 +29,37 @@
     };
   };
 
+  sops.secrets."nextcloud-admin-pass" = {};
+
+  services.nextcloud = {
+    enable = true;
+    package = pkgs.nextcloud32;
+    hostName = "cloud.snack.management";
+    https = true;
+    configureRedis = true;
+    maxUploadSize = "1G";
+    database.createLocally = true;
+    config = {
+      dbtype = "pgsql";
+      adminpassFile = config.sops.secrets."nextcloud-admin-pass".path;
+    };
+    settings = {
+      enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
+    };
+  };
+
   services.plex = {
     enable = true;
     openFirewall = true;
