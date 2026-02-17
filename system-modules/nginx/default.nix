64da7d432607db3dbd6d9fcff94057408fecb1ff
pixls <dev@snack.supply>
2026-02-17 16:02:46 -0500

    nextcloud nginx things

--- system-modules/nginx/default.nix
+++ system-modules/nginx/default.nix
@@ -90,23 +90,80 @@
       };
     in {
       "truenas.snack.management" = ortus 80;
+
       "dockge.snack.management" = john 5001;
       "prowlarr.snack.management" = john 9696;
       "sonarr.snack.management" = john 8989;
       "radarr.snack.management" = john 7878;
       "qbittorrent.snack.management" = john 8082;
-      "audiobooks.snack.management" = gideon 8000;
-      # "jellyfin.snack.management" = gideon 8096;
       "overseerr.snack.management" = john 5055;
-      # "plex.snack.management" = gideon 32400;
+      
+      "audiobooks.snack.management" = gideon 8000;
+      "cloud.snack.management" = gideon 80;
+      "immich.snack.management" = {
+        useACMEHost = "snack.management";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://192.168.1.7:2283";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+          extraConfig = ''
+            client_max_body_size 50000M;
+            proxy_read_timeout   600s;
+            proxy_send_timeout   600s;
+            send_timeout         600s;
+          '';
+        };
+      };
+      "cloud.snack.management" = {
+        useACMEHost = "snack.management";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://192.168.1.7:80";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+          extraConfig = ''
+            index index.php index.html /index.php$request_uri;
+            add_header X-Content-Type-Options nosniff;
+            add_header X-Robots-Tag "noindex, nofollow";
+            add_header X-Permitted-Cross-Domain-Policies none;
+            add_header X-Frame-Options sameorigin;
+            add_header Referrer-Policy no-referrer;
+            ${lib.optionalString (cfg.https) ''
+              add_header Strict-Transport-Security "max-age=${toString cfg.nginx.hstsMaxAge}; includeSubDomains" always;
+            ''}
+            client_max_body_size 1G;
+            fastcgi_buffers 64 4K;
+            fastcgi_hide_header X-Powered-By;
+            # mirror upstream htaccess file https://github.com/nextcloud/server/blob/v32.0.0/.htaccess#L40-L41
+            fastcgi_hide_header Referrer-Policy;
+            fastcgi_hide_header X-Content-Type-Options;
+            fastcgi_hide_header X-Frame-Options;
+            fastcgi_hide_header X-Permitted-Cross-Domain-Policies;
+            fastcgi_hide_header X-Robots-Tag;
+            gzip on;
+            gzip_vary on;
+            gzip_comp_level 4;
+            gzip_min_length 256;
+            gzip_proxied expired no-cache no-store private no_last_modified no_etag auth;
+            gzip_types application/atom+xml text/javascript application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/wasm application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
+          '';
+        };
+      };
+
+
       "hass.snack.management" = hass 8123;
       "music.snack.management" = hass 8095;
+
       "omada.snack.management" = mercymorn 8043;
+
       "acme.snack.management" = {
         forceSSL = true;
         useACMEHost = "snack.management";
         #enableACME = true;
       };
+
+      # Hosts
       "erebos.snack.management" = {
         forceSSL = true;
         useACMEHost = "snack.management";
@@ -134,36 +191,8 @@
           recommendedProxySettings = true;
         };
       };
-      "immich.snack.management" = {
-        useACMEHost = "snack.management";
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://192.168.1.7:2283";
-          proxyWebsockets = true;
-          recommendedProxySettings = true;
-          extraConfig = ''
-            client_max_body_size 50000M;
-            proxy_read_timeout   600s;
-            proxy_send_timeout   600s;
-            send_timeout         600s;
-          '';
-        };
-      };
-      # "auth.snack.management" = {
-      #   forceSSL = true;
-      #   useACMEHost = "snack.management";
-      #   locations."/" = {
-      #     proxyPass = "http://0.0.0.0:8080";
-      #     proxyWebsockets = true;
-      #     recommendedProxySettings = true;
-      #     extraConfig = ''
-      #       proxy_set_header Host $host;
-      #       proxy_set_header X-Real-IP $remote_addr;
-      #       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      #       proxy_set_header X-Forwarded-Proto https;
-      #     '';
-      #   };
-      # };
+
+      # localhost (harrowhark)
       "id.snack.management" = {
         forceSSL = true;
         useACMEHost = "snack.management";
