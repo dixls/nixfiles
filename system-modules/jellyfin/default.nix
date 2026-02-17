b5b69e7d4679869b0bf8ea5ee907f4b8b5f76e8c
pixls <dev@snack.supply>
2026-02-17 12:07:40 -0500

    jellyfin stuff

--- system-modules/jellyfin/default.nix
+++ system-modules/jellyfin/default.nix
@@ -9,4 +9,18 @@
     enable = true;
     openFirewall = true;
   };
+
+  users.users.jellyfin = {
+    uid = 333;
+  };
+
+  fileSystems."/jelly-media" = {
+    device = "//192.168.1.6/media";
+    fsType = "cifs";
+    options = [
+      "x-systemd.automount" "noauto"
+      "credentials=${config.sops.secrets."gideon-samba".path}"
+      "uid=333"
+    ];
+  };
 }
