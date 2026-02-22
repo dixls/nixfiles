62733f380ba4e876a5c3f1350a757e334904f1f7
pixls <dev@snack.supply>
2026-02-22 16:55:19 -0500

    start calibre add oidc to paperless

--- system-modules/paperless/default.nix
+++ system-modules/paperless/default.nix
@@ -8,6 +8,11 @@
     };
   };
 
+  secrets.sops."paperless-pocket-id-secret" = {
+    owner = ;
+    group = ;
+  };
+
   services.paperless = {
     enable = true;
     consumptionDirIsPublic = true;
@@ -23,8 +28,24 @@
         pdfa_image_compression = "lossless";
       };
       PAPERLESS_URL = "https://paperless.snack.management";
-      PAPERLESS_APPS="allauth.socialaccount.providers.openid_connect";
-
+      PAPERLESS_APPS = "allauth.socialaccount.providers.openid_connect";
+      PAPERLESS_SOCIALACCOUNT_PROVIDERS = {
+        openid_connect = {
+          SCOPE = [ "openid" "profile" "email" ];
+          OAUTH_PKCE_ENABLED = true;
+          APPS = [
+            {
+              provider_id = "pocket-id";
+              name = "Pocket-ID";
+              client_id = "e95134e4-1cdb-4d9d-a8c3-9a46a430af42";
+              secret = ${config.sops.secrets."paperless-pocket-id-secret"};
+              settings = {
+                server_url = "https://id.snack.management";
+              };
+            }
+          ];
+        };
+      };
     };
   };
 }
