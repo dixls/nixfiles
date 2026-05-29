0bea7e79b8f0753a22c4afe0a241884317b52a9d
pixls <dev@snack.supply>
2026-05-28 21:03:51 -0400

    reorganize and add truenas

--- system-modules/homepage/default.nix
+++ system-modules/homepage/default.nix
@@ -1,6 +1,7 @@
 {pkgs, lib, config, ...}: {
   sops.secrets.homepage-secrets = {
-    uid = 
+    uid = 0;
+    gid = 100;
   };
 
   services.homepage-dashboard = {
@@ -20,16 +21,17 @@
 
     services = [
       {
-        "Services" = [
+        "Media" = [
           {
             "Jellyfin" = {
               description = "Media server";
               href = "https://jellyfin.snack.management";
               siteMonitor = "https://jellyfin.snack.management";
+              icon = "jellyfin.png";
               widget = {
                 type = "jellyfin";
                 url = "https://jellyfin.snack.management";
-                key = "{{HOMEPAGE_VAR_JELLYFIN}}";
+                key = "{{HOMEPAGE_VAR_JELLYFIN_KEY}}";
                 version = 1;
                 enableBlocks = true;
                 enableNowPlaying = true;
@@ -38,22 +40,45 @@
               };
             };
           }
+        ];
+      }
+      {
+        "Services" = [
           {
             "Home Assistant" = {
               description = "Home automation";
               href = "https://hass.snack.management";
+              siteMonitor = "https://hass.snack.management";
+              icon = "home-assistant.png";
             };
           }
           {
             "Paperless NGX" = {
               description = "Document Management";
               href = "https://paperless.snack.management";
+              siteMonitor = "https://paperless.snack.management";
+              icon = "paperless-ngx.png";
             };
           }
           {
             "Nextcloud" = {
               description = "Cloud Storage and File Syncing";
               href = "https://cloud.snack.management";
+              siteMonitor = "https://cloud.snack.management";
+              icon = "nextcloud.png";
+            };
+          }
+          {
+            "TrueNas" = {
+              description = "Storage management";
+              href = "https://truenas.snack.management";
+              widget = {
+                type = "truenas"
+                url = "https://truenas.snack.management";
+                version = 2;
+                key = "{{HOMEPAGE_VAR_TRUENAS_KEY}}";
+                enablePools = true;
+              };
             };
           }
         ];
